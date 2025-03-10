63bf03c433148015391695725785a16766b29430
SujinC <sujinc0@gmail.com>
2025-03-10 06:11:17 +0530

    adding new project for middleware practice

---
+++ MiddleWarePractice/CustomMiddleware/MyCustomMiddWare.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MiddleWarePractice.CustomMiddleware
+{
+    public class MyCustomMiddWare : IMiddleware
+    {
+        async Task IMiddleware.InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("My Custom Middleware - Starts\n");
+            await next(context);
+            await context.Response.WriteAsync("My Custom Middleware - Ends\n");
+        }
+    }
+}
---
+++ MiddleWarePractice/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+//app.MapGet("/", () => "Hello World!");
+
+/////Middleware Chaining
+//app.Use(async (HttpContext context, RequestDelegate next) =>
+//{
+//    await context.Response.WriteAsync("First call\n");
+//    next(context);
+//});
+
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("Second call\n");
+//});
+
+//Custom Middleware
+
+builder.Services.AddTransient<IMiddleware>();
+
+
+app.Run();
